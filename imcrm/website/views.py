d8ac9af65c93e75b133dbcf74122e34dd6b74c9a
Paul Aji <paulajiparayil123@gmail.com>
2023-07-23 22:28:07 +0530

    Update views.py

--- imcrm/website/views.py
+++ imcrm/website/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+# django can handle login, logout and auth, for that import these 
+from django.contrib.auth import authenticate, login, logout
+# for flash messages when we login or logout
+from django.contrib import messages
 
 # creating homepage view
 def home(request):
     return render(request, 'home.html', {})  # {} at the end specifies we are not passing any data into the home.html
+    # also, django knows that it's home.html will be there in the templates folder that we created! kudos django
+
+# creating login view
+def login_user(request):
+    pass
+
+# creating logout view
+def logout_user(request):
+    pass
