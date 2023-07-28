0c3c538f4fe484d540063ece6bb3677e0cbcc0d1
Paul Aji <paulajiparayil123@gmail.com>
2023-07-28 12:34:57 +0530

    Update urls.py

--- imcrm/website/urls.py
+++ imcrm/website/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     # setting up the route for login and logout
     # we are calling the routes login_user and logout_user because login and logout are keywords from a library that we will import, refer to the views.py for clarification
-    path('login/', views.login_user, name='login'),
+    # path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout')
 ]
