4f8989ce89e2e8a431cc8c80b3abd8323785f71f
Paul Aji <paulajiparayil123@gmail.com>
2023-08-01 13:34:23 +0530

    Update views.py

--- imcrm/website/views.py
+++ imcrm/website/views.py
@@ -5,10 +5,20 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 # to import the form we created in forms.py (the registration/signup form)
 from .forms import SignUpForm
+# importing the Customer datamodel we just created so that we can display all the records
+from .models import Customer
+
+
 
 # creating homepage view
 # adding the login feature to the home page itself
 def home(request):
+
+
+    # grabbing all customer records to display them
+    customers = Customer.objects.all()
+
+
     # we have a form for username and password in the home page, i.e, the login form
     # in the login form if we enter something (POST) and do/invoke the POST method, we need to render 
     # if the method is GET, that is, we are not POSTing (submitting) anything, we don't need to authenticate the user or anything
@@ -27,12 +37,17 @@ def home(request):
             messages.error(request, "Please check your username and password. There was an error logging in!")
             return redirect('home')
 
+
     # till now, we have checked whether POST method was done to the login form!
     # if the method is GET, that is, we are simply visiting and not doing anything, we just render the home.html page!
     else:
-        return render(request, 'home.html', {})  
-        # {} at the end specifies we are not passing any data into the home.html
+        # passing the customers variable which contains all the customer records we inserted into the table
+        # since the method is not POST, and if we are getting the home.html rendered, it means that we are logged in and we can pass the customers variable such that all customer objects we created can be displayed
+        return render(request, 'home.html', {'customers': customers})  
+        # if {} comes at the end, it specifies we are not passing any data into the home.html
         # also, django knows that it's home.html will be there in the templates folder that we created! kudos django
+
+
 
 # creating logout view
 def logout_user(request):
@@ -42,8 +57,12 @@ def logout_user(request):
     messages.info(request, "You have been logged out!")
     return redirect('home')
 
+
+
 # creating register user view
 def register_user(request):
+
+
     if request.method == "POST":
         # by passing request.POST as parameter, the SignUpForm will be populated with the data the user is POSTing
         form = SignUpForm(request.POST)
@@ -60,6 +79,7 @@ def register_user(request):
             login(request, user)
             messages.success(request, "Registration successful!")
             return redirect('home')
+        
         
     # else, if the user is not posting and is just visiting the register page,
     else:
