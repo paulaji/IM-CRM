f89c2e3ded9333464c3e58fb5831cc1c680590bd
Paul Aji <paulajiparayil123@gmail.com>
2023-08-01 12:03:48 +0530

    Update models.py

--- imcrm/website/models.py
+++ imcrm/website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+# creating a customer data model
+class Customer(models.Model):
+    create_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=30)
+    state = models.CharField(max_length=30)
+    zip_code = models.CharField(max_length=30)
