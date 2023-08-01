7a0947a7af8951d38d2058be02aebdb1758c27a1
Paul Aji <paulajiparayil123@gmail.com>
2023-08-01 13:21:21 +0530

    Update admin.py

--- imcrm/website/admin.py
+++ imcrm/website/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+# import the Customer model we created in models.py file
+from .models import Customer
 
 # Register your models here.
+# register the Customer model
+admin.site.register(Customer)
