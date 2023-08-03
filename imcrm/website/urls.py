e7d3958a1876ba82f7815c7f1e6c078ac5b99b67
Paul Aji <paulajiparayil123@gmail.com>
2023-08-03 11:39:07 +0530

    Update urls.py

--- imcrm/website/urls.py
+++ imcrm/website/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('register/', views.register_user, name="register"),
     # when we want to specifically look at a record
     # for say, when we click on a specific record ID, it takes to that record
-    # in this, we pass in int (integer) as pk (primary key)
+    # in this, we pass in int (integer) as pk (primary key), why int? because the id of each record is an integer
     path('record/<int:pk>', views.customer_record, name="record")
 ]
