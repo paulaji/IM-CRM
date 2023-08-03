f0d2e9728fe33b673c6d8b804fa827e624ea5831
Paul Aji <paulajiparayil123@gmail.com>
2023-08-03 11:26:35 +0530

    Update urls.py

--- imcrm/website/urls.py
+++ imcrm/website/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
     # path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     # manually writing a register user functionality, we can always use the django admin panel to add more users
-    path('register/', views.register_user, name="register")
+    path('register/', views.register_user, name="register"),
+    # when we want to specifically look at a record
+    # for say, when we click on a specific record ID, it takes to that record
+    # in this, we pass in int (integer) as pk (primary key)
+    path('record/<int:pk>', views.customer_record, name="record")
 ]
