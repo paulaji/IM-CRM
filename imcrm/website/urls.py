05237a146c8f3475bef2a539dd1489a9521748f8
Paul Aji <paulajiparayil123@gmail.com>
2023-08-04 20:07:49 +0530

    Update urls.py

--- imcrm/website/urls.py
+++ imcrm/website/urls.py
@@ -16,5 +16,7 @@ urlpatterns = [
     # in this, we pass in int (integer) as pk (primary key), why int? because the id of each record is an integer
     path('record/<int:pk>', views.customer_record, name="record"),
     # path for when we want to delete a record
-    path('delete_record/<int:pk>', views.delete_record, name="delete_record")
+    path('delete_record/<int:pk>', views.delete_record, name="delete_record"),
+    # url to add new record
+    path('add_record', views.add_record, name="add_record"),
 ]
